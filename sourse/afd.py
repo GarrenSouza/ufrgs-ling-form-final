b3dbe01fe029f1a3cb4fbadc03c77ec2fa79f928
Lucas B. Rozado <lucasrozado@gmail.com>
2021-04-15 11:54:13 -0300

    Creates a Afd from a parsed input file
    From the Afd class in the afd.py file creates an afd
    using the parsed input file and the parsing function
    from the parseInput.py file

--- sourse/afd.py
+++ sourse/afd.py
@@ -1,16 +1,25 @@
-from sourse.parseInput import parseInput
+from sourse.parseInput import parseAfd
 
 class Afd:
     def __init__(self, file_path):
-        self.name = None
-        self.alpha = None
-        self.initial_state = None
-        self.final_states = None
-        self.states = None
+        dict = parseAfd(file_path)
+        self.name = dict['name']
+        self.alpha = dict['alpha']
+        self.initial_state = dict['initial']
+        self.final_states = dict['final']
+        self.states = dict['states']
+        self.prog = dict['prog']
         self.nodes = None
+    
+    def __str__(self):
+        return f'<{self.name}=(Q:{self.states}, E:{self.alpha}, {self.prog}, {self.initial_state}, F:{self.final_states})>'
+    
+    def __repr__(self):
+        return self.__str__()
 
     def minimize(self):
         pass
 
     def makeTotal(self):    
         pass
+
--- sourse/parseInput.py
+++ sourse/parseInput.py
@@ -1,9 +1,9 @@
 import re
+from pprint import pprint
 
 def parseAfd(file_path):
     file = open(file_path, encoding='utf8')
-    afd = file.readline()
-    afd = re.findall(r'(.*)=.*{(.*)},{(.*)},(.*),(.*),{(.*)}', afd)
-    return afd[0]
+    afd = re.findall(r'(.*)=.*{(.*)},{(.*)},(.*),(.*),{(.*)}', file.readline())[0]
+    file.close()
+    return {'name':afd[0], 'states':afd[1].split(','), 'alpha':afd[2].split(','), 'prog':afd[3], 'initial':afd[4], 'final':afd[5].split(',')}
 
-parseAfd('input.txt')
